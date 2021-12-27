ed1ebfac25f6a957063fad763a21f73d28b616a2
Spencer Powers <54383192+scpowers@users.noreply.github.com>
2021-12-27 11:38:21 -1000

    First pass at adding system class attributes, added Eigen dependency

--- System.h
+++ System.h
@@ -4,9 +4,33 @@
 
 #ifndef FASTDDP_SYSTEM_H
 #define FASTDDP_SYSTEM_H
+#include <Eigen/Dense>
+#include <vector>
 
+using Eigen::MatrixXd;
 
 class System {
+private:
+    float Ceiling_H;
+    float tf;
+    float N_seg;
+    float dt;
+    MatrixXd Q;
+    MatrixXd R;
+    MatrixXd Qf;
+    //std::function<auto(_some_local_struct_)> f;
+    //std::function<auto(_some_local_struct_)> L;
+    //std::function<auto(_some_local_struct_)> Lf;
+    float mu;
+    std::vector<float> xd;
+    std::vector<float> xmin;
+    std::vector<float> xmax;
+    float lambda;
+    // some struct vector to hold some obstacles
+    float ko;
+    std::vector<float> umin;
+    std::vector<float> umax;
+
 
 };
 
--- main.cpp
+++ main.cpp
@@ -3,9 +3,19 @@
  */
 
 #include <iostream>
+#include <Eigen/Dense>
+#include "System.h"
 
-int main() {
-    std::cout << "Testing System class..." << std::endl;
+using Eigen::MatrixXd;
+
+int main()
+{
+    MatrixXd m(2,2);
+    m(0,0) = 3;
+    m(1,0) = 2.5;
+    m(0,1) = -1;
+    m(1,1) = m(1,0) + m(0,1);
+    std::cout << m << std::endl;
 
     return 0;
 }
