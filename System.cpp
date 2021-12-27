01216fbd1027afb51e8c2c26b0273ca8299bc9c9
Spencer Powers <54383192+scpowers@users.noreply.github.com>
2021-12-27 10:23:46 -1000

    Initialized System class files

---
+++ System.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Spencer Powers on 12/27/21.
+//
+
+#include "System.h"
---
+++ System.h
@@ -0,0 +1,14 @@
+//
+// Created by Spencer Powers on 12/27/21.
+//
+
+#ifndef FASTDDP_SYSTEM_H
+#define FASTDDP_SYSTEM_H
+
+
+class System {
+
+};
+
+
+#endif //FASTDDP_SYSTEM_H
