ee4ad32ec132d0ce08fb28ad2a2c8537c9d160ce
Spencer Powers <54383192+scpowers@users.noreply.github.com>
2022-01-01 16:22:55 -1000

    Improved commenting

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ using Eigen::MatrixXd;
 
 int main()
 {
+    // checking Eigen imported correctly
     MatrixXd m(2,2);
     m(0,0) = 3;
     m(1,0) = 2.5;
@@ -17,12 +18,14 @@ int main()
     m(1,1) = m(1,0) + m(0,1);
     std::cout << m << std::endl;
 
+    // checking attributes of S
     System S;
-
+    // adding a simple lambda func for f attribute (dynamics)
     std::function<int(int)> f = [&] (int x) {return 2*x;};
     S.setf(f);
     std::function<int(int)> fcheck = S.getf();
     int y = fcheck(3);
+    std::cout << y << std::endl;
 
     return 0;
 }
